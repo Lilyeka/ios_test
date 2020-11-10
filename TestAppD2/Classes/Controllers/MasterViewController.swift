0875abeea74572cf776b30d928cff2a03659c1b2
Liliya <lilyeka@gmail.com>
2020-11-10 13:38:11 +0200

    Устранена ошибка с необновлением тега запроса после переключения тега. Дублирующиеся, а также длинные куски кода, вынесены в отдельные методы.

--- TestAppD2/Classes/Controllers/MasterViewController.swift
+++ TestAppD2/Classes/Controllers/MasterViewController.swift
@@ -34,16 +34,31 @@ class MasterViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         tableView.register(UINib(nibName: "QuestionTableViewCell", bundle: nil), forCellReuseIdentifier: kCellIdentifier)
-        
         addRefreshControlOnTabelView()
         settingDynamicHeightForCell()
         addActivityIndicator()
-       
-        NotificationCenter.default.addObserver(self, selector: #selector(self.requestedTagNotification(_:)), name: NSNotification.Name.requestedTagNotification, object: nil)
+        addNotificationObserver()
         requestedTag = ArrayOfTags.shared[0]
         definesPresentationContext = true
         questions = [Item]()
-        
+        loadQuestions()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indexPath: IndexPath? = tableView.indexPathForSelectedRow
+        let detailViewController = (segue.destination as? UINavigationController)?.topViewController as? DetailViewController
+        let item = questions?[indexPath?.row ?? 0]
+        detailViewController?.currentQuestion = item
+        detailViewController?.loadAnswers()
+        detailViewController?.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+        detailViewController?.navigationItem.leftItemsSupplementBackButton = true
+    }
+    
+    // MARK: - Private methods
+    private func addNotificationObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.requestedTagNotification(_:)), name: NSNotification.Name.requestedTagNotification, object: nil)
+    }
+    private func loadQuestions() {
         numberOfPageToLoad = 1
         self.activityIndicatorView.startAnimating()
         questionSevice.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { [unowned self] (questions, error) in
@@ -59,17 +74,28 @@ class MasterViewController: UIViewController {
         numberOfPageToLoad += 1
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let indexPath: IndexPath? = tableView.indexPathForSelectedRow
-        let detailViewController = (segue.destination as? UINavigationController)?.topViewController as? DetailViewController
-        let item = questions?[indexPath?.row ?? 0]
-        detailViewController?.currentQuestion = item
-        detailViewController?.loadAnswers()
-        detailViewController?.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-        detailViewController?.navigationItem.leftItemsSupplementBackButton = true
+    private func loadModeQuestions() {
+        loadMoreStatus = true
+        let bounds: CGRect = UIScreen.main.bounds
+        activityIndicatorView.center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height - 50)
+        activityIndicatorView.startAnimating()
+        questionSevice.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (questions, error) in
+            if let questions = questions {
+                for q in questions {
+                    self.questions?.append(q)
+                }
+                self.tableView.reloadData()
+                self.numberOfPageToLoad += 1
+                self.loadMoreStatus = false
+            }
+            if !error.isEmpty {
+                self.showErrorAlert(errorMessage: error)
+            }
+            self.activityIndicatorView.stopAnimating()
+            self.activityIndicatorView.center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height / 2)
+        }
     }
     
-    // MARK: - Private methods
     private func addRefreshControlOnTabelView() {
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(self.reloadData), for: .valueChanged)
@@ -98,30 +124,19 @@ class MasterViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "ОК", style: .cancel, handler: nil))
         self.present(alert, animated: true)
     }
+    
     // MARK: - Actions
     @objc func reloadData() {
-        numberOfPageToLoad = 1
-        questionSevice.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) {
-            [unowned self] (questions, error) in
-            if let questions = questions {
-                self.questions = questions
-                self.tableView.reloadData()
-            }
-            if !error.isEmpty {
-                self.showErrorAlert(errorMessage: error)
-            }
-            self.activityIndicatorView.stopAnimating()
-            if self.refreshControl != nil {
-                let formatter = DateFormatter()
-                formatter.dateFormat = "MMM d, h:mm a"
-                let title = "Last update: \(formatter.string(from: Date()))"
-                let attrsDictionary = [NSAttributedString.Key.foregroundColor : UIColor.black]
-                let attributedTitle = NSAttributedString(string: title, attributes: attrsDictionary)
-                self.refreshControl?.attributedTitle = attributedTitle
-                self.refreshControl?.endRefreshing()
-            }
+        loadQuestions()
+        if self.refreshControl != nil {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MMM d, h:mm a"
+            let title = "Last update: \(formatter.string(from: Date()))"
+            let attrsDictionary = [NSAttributedString.Key.foregroundColor : UIColor.black]
+            let attributedTitle = NSAttributedString(string: title, attributes: attrsDictionary)
+            self.refreshControl?.attributedTitle = attributedTitle
+            self.refreshControl?.endRefreshing()
         }
-        numberOfPageToLoad += 1
     }
        
     //MARK: -UIScrollViewDelegate
@@ -129,44 +144,14 @@ class MasterViewController: UIViewController {
         let actualPosition: CGFloat = scrollView.contentOffset.y
         let contentHeight: CGFloat = scrollView.contentSize.height - scrollView.frame.size.height
         if actualPosition >= contentHeight && actualPosition > 0 && loadMoreStatus == false {
-            let bounds: CGRect = UIScreen.main.bounds
-            activityIndicatorView.center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height - 50)
-            activityIndicatorView.startAnimating()
-            loadMoreStatus = true
-            questionSevice.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (questions, error) in
-                if let questions = questions {
-                    for q in questions {
-                        self.questions?.append(q)
-                    }
-                    self.tableView.reloadData()
-                    self.numberOfPageToLoad += 1
-                    self.loadMoreStatus = false
-                }
-                if !error.isEmpty {
-                    self.showErrorAlert(errorMessage: error)
-                }
-                self.activityIndicatorView.stopAnimating()
-                self.activityIndicatorView.center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height / 2)
-            }
+            loadModeQuestions()
         }
     }
 
     // MARK: - Notification
     @objc func requestedTagNotification(_ notification: Notification?) {
-        activityIndicatorView.startAnimating()
-        let requestedTag = notification?.object as? String
-        numberOfPageToLoad = 1
-        questionSevice.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (questions, error) in
-            if let questions = questions {
-                self.questions = questions
-                self.tableView.reloadData()
-            }
-            if !error.isEmpty {
-                self.showErrorAlert(errorMessage: error)
-            }
-            self.activityIndicatorView.stopAnimating()
-        }
-        numberOfPageToLoad += 1
+        requestedTag = notification?.object as? String ?? ""
+        loadQuestions()
     }
     
     // MARK: - IBAction
